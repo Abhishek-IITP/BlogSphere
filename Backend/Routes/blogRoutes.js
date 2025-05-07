a3891981470613024c2e9ea3a0b2fd28f2eb6054
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-07 10:34:59 +0530

    Routes

--- Backend/Routes/blogRoutes.js
+++ Backend/Routes/blogRoutes.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const {createBlogs , getBlogs, getBlogsById, updateBlogs, deleteBlogs,likeBlogs} = require('../controllers/blogController');
+const {createBlogs , getBlogs, getBlogsById, updateBlogs, deleteBlogs,likeBlog,saveBlog} = require('../controllers/blogController');
 const verifyUser = require('../Middlewares/auth');
 const { addComment, deleteComment, editComment, likeComment } = require('../controllers/commentController');
+const upload = require('../Utils/multer');
 
 const route= express.Router();
 
-route.post("/blogs", verifyUser ,createBlogs);
+route.post("/blogs", verifyUser, upload.fields([{name: 'image', maxCount: 1}, {name: 'images'}]) ,createBlogs);
+// route.post("/blogs", verifyUser, upload.single("image"),upload.array("images") ,createBlogs);
 
 route.get("/blogs",getBlogs );
 
-route.get("/blogs/:id",getBlogsById);
+route.get("/blogs/:blogId",getBlogsById);
 
-route.patch("/blogs/:id",verifyUser,updateBlogs);
+route.patch("/blogs/:id",verifyUser, upload.fields([{name: 'image', maxCount: 1}, {name: 'images'}]) ,updateBlogs);
 
 route.delete("/blogs/:id",verifyUser, deleteBlogs);
 
 
-route.post("/blogs/like/:id",verifyUser, likeBlogs);
+route.post("/blogs/like/:id",verifyUser, likeBlog);
 
 route.post("/blogs/comment/:id",verifyUser, addComment);
 
@@ -26,6 +28,10 @@ route.patch("/blogs/edit-comment/:id",verifyUser, editComment);
 
 
 route.patch("/blogs/like-comment/:id",verifyUser, likeComment);
+
+// SAVE BLOGS / BOOKMARK BLOGS
+
+route.patch("/save-blog/:id",verifyUser,saveBlog)
 
 
 module.exports=route
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const {createUser , getAllUsers, deleteUser, updateUser, getAllUsersById, login} = require('../controllers/userController');
-const User= require('../models/userSchema')
+const {createUser , getAllUsers, deleteUser, updateUser, getAllUsersById, login,  googleAuth , verifyEmail} = require('../controllers/userController');
+const verifyUser = require('../Middlewares/auth');
+// const User= require('../models/userSchema')
 const route= express.Router();
 
 
 
-route.post("/users", createUser )
-route.post("/login", login )
+route.post("/signup", createUser )
+route.post("/signin", login )
 
 route.get("/users",getAllUsers);
 
 route.get("/users/:id",getAllUsersById);
 
-route.patch("/users/:id",updateUser);
+route.patch("/users/:id",verifyUser,updateUser);
 
-route.delete("/users/:id", deleteUser);
+route.delete("/users/:id",verifyUser, deleteUser);
+
+
+route.get("/verify-email/:verificationToken" ,verifyEmail)
+
+// googleAuth
+// route.get("/google-auth" ,googleAuth)
+route.post("/google-auth", googleAuth)
 
 
 
