c946dbe4fc6c9c80c4afbfb5114016f000633bff
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-14 12:41:29 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+        },
+      },
+    },
+  };
+  
