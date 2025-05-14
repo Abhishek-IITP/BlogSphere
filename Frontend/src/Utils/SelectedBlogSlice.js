7438a0a611f7df1ec447d4bff4fd8d28976d1c3a
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-14 12:42:09 +0530

    SelectedBlog Slice

--- Frontend/src/Utils/SelectedBlogSlice.js
+++ Frontend/src/Utils/SelectedBlogSlice.js
@@ -8,11 +8,11 @@ const SelectedBlogSlice = createSlice({
     comments: [],
   },
   reducers: {
-    addSelectedBlog(state, action) {
+    addSelectedBlog(state, action) { 
       localStorage.setItem("SelectedBlog", JSON.stringify(action.payload));
       return action.payload;
     },
-    removeSelectedBlog(state,action) {
+    removeSelectedBlog(state, action) {
       localStorage.removeItem("SelectedBlog");
       return {};
     },
@@ -20,41 +20,103 @@ const SelectedBlogSlice = createSlice({
       if (state.likes.includes(action.payload)) {
         state.likes = state.likes.filter((like) => like !== action.payload);
       } else {
-        state.likes = [...state.likes , action.payload];
+        state.likes = [...state.likes, action.payload];
       }
-      localStorage.setItem("SelectedBlog", JSON.stringify(state));
+
       return state;
     },
+
     setComments(state, action) {
       state.comments = [...state.comments, action.payload];
-      localStorage.setItem("SelectedBlog", JSON.stringify(state));
     },
+ 
     setCommentLikes(state, action) {
-      // let { commentId, userId } = action.payload;
-      // function toogleLike(comments){
-      //   return comments.map((commnet)=>{
-      //     if(comment._id == commentId){
-      //       if(comment.likes.includes(userId)){
-      //         return comment;
-      //       }
-      //       else{
-      //         comment.likes = [...comment.likes, userId]
-      //       }
-      //     }
+      let { commentId, userId } = action.payload;
+      function toogleLike(comments) {
+        return comments.map((comment) => {
+          if (comment._id == commentId) {
+            if (comment.likes.includes(userId)) {
+              comment.likes = comment.likes.filter((like) => like !== userId);
+              return comment;
+            } else {
+              comment.likes = [...comment.likes, userId];
+              return comment;
+            }
+          }
 
-      //   })
-      // }
-      const { commentId, userId } = action.payload;
-      const comment = state.comments?.find(comment => comment._id === commentId);
-      if (comment) {
-        if (!comment.likes) comment.likes = [];
-        if (comment.likes.includes(userId)) {
-          comment.likes = comment.likes.filter(like => like !== userId);
-        } else {
-          comment.likes.push(userId);
-        }
-        localStorage.setItem("SelectedBlog", JSON.stringify(state));
+          if (comment.replies && comment.replies.length > 0) {
+            return { ...comment, replies: toogleLike(comment.replies) };
+          }
+
+          return comment;
+        });
       }
+      state.comments = toogleLike(state.comments);
+    },
+    setReplies(state, action) {
+      let newReply = action.payload;
+
+      function findParentComment(comments) {
+        let parentComment;
+
+        for (const comment of comments) {
+          if (comment._id === newReply.parentComment) {
+            parentComment = {
+              ...comment,
+              replies: [...comment.replies, newReply],
+            };
+            break;
+          }
+
+          if (comment.replies.length > 0) {
+            parentComment = findParentComment(comment.replies);
+            if (parentComment) {
+              parentComment = {
+                ...comment,
+                replies: comment.replies.map((reply) =>
+                  reply._id == parentComment._id ? parentComment : reply
+                ),
+              };
+              break;
+            }
+          }
+        }
+
+        return parentComment; //top level comment return kr raha hu dost ;
+      }
+
+      let parentComment = findParentComment(state.comments);
+
+      state.comments = state.comments.map((comment) =>
+        comment._id == parentComment._id ? parentComment : comment
+      );
+    },
+
+    setUpdatedComments(state, action) {
+      function updateComment(comments) {
+        return comments.map((comment) =>
+          comment._id == action.payload._id
+            ? { ...comment, comment: action.payload.comment }
+            : comment.replies && comment.replies.length > 0
+            ? { ...comment, replies: updateComment(comment.replies) }
+            : comment
+        );
+      }
+
+      state.comments = updateComment(state.comments);
+    },
+    deleteCommentAndReply(state, action) {
+      function deleteComment(comments) {
+        return comments
+          .filter((comment) => comment._id !== action.payload)
+          .map((comment) =>
+            comment.replies && comment.replies.length > 0
+              ? { ...comment, replies: deleteComment(comment.replies) }
+              : comment
+          );
+      }
+
+      state.comments = deleteComment(state.comments);
     },
 
 
@@ -76,7 +138,10 @@ export const {
   changeLikes,
   changeSaves,
   setComments,
-  setCommentLikes
+  setCommentLikes,
+  setReplies,
+  deleteCommentAndReply,
+  setUpdatedComments,
 } = SelectedBlogSlice.actions;
 
 export default SelectedBlogSlice.reducer;
