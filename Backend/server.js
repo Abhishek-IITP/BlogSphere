6095a9f1a3cc57cea75958b837383491022d6acc
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-15 21:10:22 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,12 +10,17 @@ const cloudinaryConfig = require("./Config/cloudinaryConfig");
 require ('dotenv').config()
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/v1",userRoutes)
 app.use("/api/v1",blogRoutes)
 
+app.get("/",(req,res)=>{
+  res.send("Hello jii...kya haal h")
+})
 // Server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
