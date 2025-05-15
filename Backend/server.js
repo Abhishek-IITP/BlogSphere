4a0915f074677082cb92122ecd733ad2b2285808
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-15 12:27:48 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,10 @@ const app = express();
 const userRoutes= require("./Routes/userRoutes")
 const blogRoutes= require("./Routes/blogRoutes")
 const mongoose = require("mongoose");
-const User=require("./models/userSchema")
+const User=require("./models/userSchema");
+const cloudinaryConfig = require("./Config/cloudinaryConfig");
+require ('dotenv').config()
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,4 +20,5 @@ app.use("/api/v1",blogRoutes)
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   dbConnect();
+  cloudinaryConfig();
 });
