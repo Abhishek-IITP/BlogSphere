425601107c8bd50938b07ed47ebb0e12a4248f94
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-16 12:55:55 +0530

    Create tailwind.config.js

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+        },
+        plugins: [require('tailwind-scrollbar-hide')],
+      },
+    },
+  };
+  
